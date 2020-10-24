fceb22535c1b3eb6a4c639c8827d02f08950f2d0
copito.123 <nicolspnicolsp@gmail.com>
2020-10-24 11:51:32 -0300

    cuarto  commit

--- app/src/main/java/com/example/phone/FirstFragment.kt
+++ app/src/main/java/com/example/phone/FirstFragment.kt
@@ -1,17 +1,32 @@
 package com.example.phone
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.phone.local.RealPhone
+import com.example.phone.viewModel.PhoneAdapter
+import com.example.phone.viewModel.PhoneViewModel
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(),PhoneAdapter.PassTheData {
+lateinit var  mAdapter: PhoneAdapter
+    private val model: PhoneViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAdapter = PhoneAdapter(this)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +39,23 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        val recyclerView = mRecycler
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        model.exposeLiveDataFromDataBase().observe(viewLifecycleOwner, Observer {
+            Log.d("VIEW",it.toString())
+            mAdapter.updateAdapter(it)
+        })
+
+       // view.findViewById<Button>(R.id.button_first).setOnClickListener {
+         //   findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+       // }
+    }
+
+    override fun passTheData(phone: RealPhone) {
+        val bundle = Bundle()
+        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
     }
 }
--- app/src/main/java/com/example/phone/viewModel/PhoneAdapter.kt
+++ app/src/main/java/com/example/phone/viewModel/PhoneAdapter.kt
@@ -1,4 +1,48 @@
 package com.example.phone.viewModel
 
-class PhoneAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.phone.R
+import com.example.phone.local.RealPhone
+import kotlinx.android.synthetic.main.phone_list_view.view.*
+
+class PhoneAdapter( val mPassTheData: PassTheData): RecyclerView.Adapter<PhoneAdapter.PhoneViewHolder>() {
+    private var phoneinList = emptyList<RealPhone>()
+    fun updateAdapter(mList: List<RealPhone>) {
+        phoneinList = mList
+        notifyDataSetChanged()
+
+    }
+
+    inner class PhoneViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val id = itemView.tv1
+        val itemImage = itemView.imageView
+        val name = itemView.tv2
+        val price = itemView.tv3
+        val clickListener = itemView.setOnClickListener {
+            mPassTheData.passTheData(phoneinList[adapterPosition])
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhoneViewHolder {
+        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.phone_list_view, parent, false)
+        return PhoneViewHolder(itemView)
+    }
+    override fun getItemCount() = phoneinList.size
+
+    interface PassTheData {
+        fun passTheData(phone: RealPhone)
+
+    }
+    override fun onBindViewHolder(holder: PhoneViewHolder,position: Int) {
+        val phone = phoneinList[position]
+        Glide.with(holder.itemView.context).load(phone.image).into(holder.itemImage)
+        holder.name.text = phone.name
+        holder.price.text = phone.price.toString()
+
+    }
 }
